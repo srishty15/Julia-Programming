3613a0870e8aae8d3e0f58c8393f1072d6ed52cc
Srishty <srishty1510@gmail.com>
2018-08-25 00:49:40 +0530

    Update 11. For Loop.jl

--- 11. For Loop.jl
+++ 11. For Loop.jl
@@ -13,4 +13,7 @@ for i=1:2 , j=3:4            # Nested for
   print(i,j)                 # will print 13142324     
 end
   
+for i in [1,2,3,4,5]         # loop in an array; it will access all the elements in arrays
+  print(i)                   # will print 12345
+end
 # in all above cases, i and j are Local variables and will generate an error if they are accessed outside the body of loop
