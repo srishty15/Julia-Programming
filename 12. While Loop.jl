1b9f98ab39310fc69dd7de015e558f959fa46bc8
Srishty <srishty1510@gmail.com>
2018-08-25 00:48:00 +0530

    Update 12. While Loop.jl

--- 12. While Loop.jl
+++ 12. While Loop.jl
@@ -1,1 +1,14 @@
+# While Loop
+# Syntax
+global var = any_initial_value
+while cond
+  .....
+  .....
+  global var = var+1       # if not declared global, will create an error
+  end
 
+global i= 1
+while i<5
+  print(i)                 # will print 1234
+  global i = i+1
+end
