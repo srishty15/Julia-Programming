7b82d923d481d46d5c9545006b9ebd1dbcd8bf79
Srishty <srishty1510@gmail.com>
2018-08-24 23:45:17 +0530

    Update 8. Conditional Expression.jl

--- 8. Conditional Expression.jl
+++ 8. Conditional Expression.jl
@@ -2,7 +2,14 @@
 
 x=10
 y=20
+# using if-else
+if x >= y
+   print(x," is greater than ", y)
+else
+   print(y," is greater than ", x)
+ end                        # condition will end here  
  
+# using elif 
 if x > y
    print(x," is greater than ", y)
 elseif x < y
