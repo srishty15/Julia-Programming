27b924926d43ed378ca1f1991d77b8d5d1c19640
Srishty <srishty1510@gmail.com>
2018-08-24 22:42:22 +0530

    Update Rational Numbers.jl

--- Rational Numbers.jl
+++ Rational Numbers.jl
@@ -1,1 +1,16 @@
+# Rationals are constructed using the // operator
+x = 2 // 3
+typeof(x)  # will print Rational{Int64}
 
+#If the numerator and denominator of a rational have common factors, they are reduced to lowest terms such that the denominator is non-negative
+6 // 9   # will print 2//3
+
+numerator(5//6)   # will print 5
+denominator(5//6) # will print 6
+
+# Constructing infinite rational values is acceptable
+5//0  # will give no error
+
+# all relational and arithmetic operations can be performed on rtional numbers
+
+float(5//10)  # will return 0.2
