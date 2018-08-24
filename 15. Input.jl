b972537b69d3494d3e5c986d988d4fef2d1dfec4
Srishty <srishty1510@gmail.com>
2018-08-25 04:34:52 +0530

    Update 15. Input.jl

--- 15. Input.jl
+++ 15. Input.jl
@@ -1,6 +1,10 @@
 # taking input from users
 # for strings
-
 var = readline()
 typeof(var)          # will print String
 # by default any input in Julia is a String Input
+
+# for numbers
+var = parse(Int , readline())
+typeof(var)          # will print Int64
+# the value inputted from readline() is parsed to Int type 
