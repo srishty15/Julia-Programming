de605bc1b3a7eb36cc06d982acd8447a39d99f66
Srishty <srishty1510@gmail.com>
2018-08-24 22:31:11 +0530

    Create Complex Numbers.jl

---
+++ Complex Numbers.jl
@@ -0,0 +1,10 @@
+# Complex Numbers
+# the imaginary part in Julia is denoted by 'im'
+x = 1+2m
+typeof(x)  # will print Complex{Int64}
+(2 + 4im) + (4 + 6im)
+(2 + 4im) - (4 + 6im)
+(2 + 4im) / (4 + 6im)
+(2 + 4im) * (4 + 6im)
+(2 + 4im)^2
+3(2 + 4im)  # multiply 3 by both 2 and 4im
